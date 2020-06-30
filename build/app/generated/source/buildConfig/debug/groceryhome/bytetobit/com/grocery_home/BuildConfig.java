7680dca8dc372ef384bc80c3a5ed106c3c662e1f
SonicFan12377 <alizapp44@gmail.com>
2020-06-30 16:17:38 -0400

    Orginial Version: https://github.com/SonicFan12377/Flutter-Grocery-Home

---
+++ build/app/generated/source/buildConfig/debug/groceryhome/bytetobit/com/grocery_home/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package groceryhome.bytetobit.com.grocery_home;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mfshopping.grocery_home";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
